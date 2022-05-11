c35fbcde8a1377501ed70b51b2373f067be254f9
Thom-Ri <mune.thom2@gmail.com>
2022-05-11 16:16:46 -0300

    Correccion de desafio 4

--- clase4/clase4.js
+++ clase4/clase4.js
@@ -1,8 +1,9 @@
-const fs = require('fs')
+const fs = require('fs');
+const { json } = require('stream/consumers');
 
 class Contenedor{
-    constructor(archivo,objeto){
-        this.archivo = archivo
+    constructor(objeto){
+        this.archivo = './archivo.txt'
         this.objeto = objeto
     }
     save(){ 
@@ -10,51 +11,68 @@ class Contenedor{
         this.objeto.forEach(element => {
             element = { ...element, "id": contador }
             contador ++
-            fs.appendFileSync(this.archivo, JSON.stringify(element))
-            return console.log(element.id)    
+            fs.appendFile(this.archivo, JSON.stringify(element), err =>{
+                if(err){
+                    console.log("Problemas escribiendo el archivo")
+                }else{
+                    return console.log(`Se guardo el elemento ${element.id}`)
+                }
+            })
         });
     }
-    getById(){
+    getById(id){
         let archivoCompleto = []
-        let data = fs.readFileSync(this.archivo, "utf-8")
-        JSON.parse(JSON.stringify(data))
-        archivoCompleto.push(data)
-        let FoundItem = archivoCompleto.filter((item) => item.id == 1)
-        console.log(FoundItem)
+        fs.readFile(this.archivo, "utf-8", (err,contenido) =>{
+            if(err){
+                console.log("Problemas en el getByID")
+            }else{
+                const content = JSON.parse(contenido)
+                archivoCompleto.push(content)
+                let FoundItem = archivoCompleto.find((item) => item.id != id);
+                console.log(`Este es el archivo encontrado`)
+                console.log(FoundItem)
+            }
+        })
+        
     }
-     getAll(){
-        const data = fs.promises.readFile(this.archivo, "utf-8")
-        .then(contenido =>{
-            console.log(contenido)
-            console.log("Se leyó el archivo con exito")
+    getAll(){
+       const data = fs.readFile(this.archivo, "utf-8", (err,contenido) =>{
+           if (err){
+               console.log(`error de lectura: ` + err)
+           }
+           else{
+               console.log(contenido)
+               console.log("Se leyó el archivo con exito")
+           }
+       })
+    } 
+    deleteById(id){
+        fs.readFile(this.archivo, "utf-8",(err, contenido) =>{
+            let archivoCompleto = []
+            if(err){
+                console.log("Problemas deleting by ID")
+            }else{
+                const content = JSON.parse(contenido)
+                console.log('type is: ' + typeof(content))
+                console.log('befpre:' + content)
+                archivoCompleto.push(content)
+                var newArchivo = archivoCompleto.filter((item) => item.id != id);
+                console.log('after:' + newArchivo)
+                fs.writeFileSync(this.archivo, JSON.stringify(newArchivo))
+
+            }
         })
-        .catch(err =>{
-            console.log(`error de lectura: ` + err)
-        })
-     } 
-    deleteById(){
-        let archivoCompleto = []
-        const data = fs.readFileSync(this.archivo, "utf-8")
-        archivoCompleto.push(data)
-        var newArchivo = archivoCompleto.filter((item) => item.id != 1);
-        archivoCompleto = newArchivo
-        fs.writeFileSync(this.archivo, JSON.stringify(archivoCompleto))
-    
+        
     }
     deleteAll(){
-        fs.promises.unlink(this.archivo)
-        .then(
-            console.log("el archivo fue eliminidado")
-        )
-        .catch(
-            console.log("el archivo no fue eliminado")
-        )
+        fs.unlinkSync(this.archivo)
+        console.log("archivo eliminado")
     }
 }
 
-let jazmin = new Contenedor('./archivo.txt',[{titulo:"Regla Flexible",precio: 550, url:"lorem"},{titulo:"tijera",precio: 250, url:"lorem"},{titulo:"Lapiz",precio: 150, url:"lorem"}])
-console.log(jazmin.save())
-console.log(jazmin.getAll())
-console.log(jazmin.getById())
-console.log(jazmin.deleteById())
-console.log(jazmin.deleteAll())
+let jazmin = new Contenedor([{titulo:"Regla Flexible",precio: 550, url:"lorem"},{titulo:"tijera",precio: 250, url:"lorem"},{titulo:"Lapiz",precio: 150, url:"lorem"}])
+jazmin.save()
+jazmin.getAll()
+jazmin.getById(2)
+jazmin.deleteById(0)
+jazmin.deleteAll()
