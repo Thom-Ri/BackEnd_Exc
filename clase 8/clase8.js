32c4ae8e0a8f31190e74629ec8d38808c3741934
Thom-Ri <mune.thom2@gmail.com>
2022-05-25 20:23:47 -0300

    Correccion clase 8

--- clase 8/clase8.js
+++ clase 8/clase8.js
@@ -23,26 +23,54 @@ router.get('', (req, res) => {
 })
 
 router.post('', (req, res) => {
-    const data = fs.readFile('./productos.txt', "utf-8")
-    const content = JSON.parse(data)
     const newProduct = req.body
-    newProduct.id = content.lengt + 1
-    fs.appendFileSync('./productos.txt', newProduct)
-    res.status(200).send(newProduct);
+    fs.readFile('./productos.txt', "utf-8", (error, contenido) =>{
+        if(error){
+            console.log("hay errores")
+        }else{
+            const content = JSON.parse(contenido)
+            console.log(newProduct)
+            fs.appendFileSync('./productos.txt', newProduct)
+            res.status(200).json(newProduct);
+        }
+    })
 })
 
 
- 
 
 router.get('/:id', (res,req)=>{
+    fs.readFile('./productos.txt', "utf-8", (err,contenido) =>{
+        if(err){
+            console.log("Problemas en el getByID")
+        }else{
+            const content = JSON.parse(contenido)
+            let FoundItem = content.find((item) => item.id == req.param.id);
+            console.log(FoundItem)
+            return FoundItem
+        }
+    })
+})
+
+router.put('/:id', (res,req)=>{
     fs.readFile('./Productos.txt', "utf-8", (err,contenido) =>{
         if(err){
             console.log("Problemas en el getByID")
         }else{
             const content = JSON.parse(contenido)
             let FoundItem = content.find((item) => item.id ==  req.params.id);
-            console.log(FoundItem)
-            res.send(FoundItem)
+        }
+    })
+})
+router.delete('/:id', (res,req)=>{
+    fs.readFile('./Productos.txt', "utf-8",(err, contenido) =>{
+        let archivoCompleto = []
+        if(err){
+            console.log("Problemas deleting by ID")
+        }else{
+            const content = JSON.parse(contenido)
+            var newArchivo = content.filter((item) => item.id != req.params.id);
+            archivoCompleto.push(newArchivo)
+            fs.writeFileSync('./Productos.txt', JSON.stringify(archivoCompleto))
         }
     })
 })
